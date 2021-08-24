e1a2748e7c43cd641d8d47226a34a75a7789eb34
Ghyath Noureddine <ghyathnour@gmail.com>
2021-08-24 11:39:22 +0300

    Add SQL Queries Folder / Assignment 3

---
+++ SQL Queries/Queries.sql
@@ -0,0 +1,20 @@
+# 1 . Select all the degrees of all the instructors in this university
+
+SELECT *
+FROM degree;
+
+# 2 . Select the first name of the instructors who earned an MS degree in Computer Science
+
+SELECT i.first_name
+FROM instructor as i,degree as d
+WHERE d.date = (SELECT MIN(date) FROM degree WHERE degree_type = "MS" AND field = "Computer Science") AND i.instructor_id = d.instructor_instructor_id; 
+
+# 3 . Delete all instructors
+
+DELETE FROM instructor;
+
+# 4 . Add instructor
+
+INSERT INTO `instructor`(`instructor_id`, `first_name`, `last_name`, `faculty_faculty_id`) VALUES (NULL,'NEW','Instructor',1)
+
+
