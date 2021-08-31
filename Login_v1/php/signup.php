6fc594dad1e09e86b459ee1766d2b120f693e9dc
Ghyath Noureddine <ghyathnour@gmail.com>
2021-08-31 13:32:23 +0300

    Add email already exists checker

--- Login_v1/php/signup.php
+++ Login_v1/php/signup.php
@@ -3,20 +3,30 @@
 include "./config.php";
 session_start();
 
-echo $_POST["email"], $_POST["pass"], $_POST["firstName"], $_POST["lastName"], $_POST["gender"];
-
 if (isset($_POST["email"]) && isset($_POST["pass"])
     && isset($_POST["firstName"]) && isset($_POST["lastName"]) && isset($_POST["gender"])) {
 
-    if ($stmt = $connection->prepare("INSERT INTO user (`email`, `password`, `first_name`, `last_name`, `gender`) VALUES (?, ?, ?, ?, ?)")) {
-        $stmt->bind_param("sssss", $_POST["email"], hash('sha256', $_POST["pass"]), $_POST["firstName"], $_POST["lastName"], $_POST["gender"]);
+    if ($stmt = $connection->prepare('SELECT email FROM user WHERE email = ?')) {
+        $stmt->bind_param('s', $_POST['email']);
         $stmt->execute();
-    }
+        $stmt->store_result();
 
-    if ($stmt->affected_rows > 0) {
-        header("location: ../index.html");
-    } else {
-        echo $stmt->error;
-        echo 'Incorrect username and/or password!';
+        if ($stmt->num_rows > 0) {
+            echo ("Email already exists, Try to login");
+            header("refresh:2;url=../index.html");
+        } else {
+            if ($stmt = $connection->prepare("INSERT INTO user (`email`, `password`, `first_name`, `last_name`, `gender`) VALUES (?, ?, ?, ?, ?)")) {
+                $stmt->bind_param("sssss", $_POST["email"], hash('sha256', $_POST["pass"]), $_POST["firstName"], $_POST["lastName"], $_POST["gender"]);
+                $stmt->execute();
+            }
+
+            if ($stmt->affected_rows > 0) {
+                header("location: ../index.html");
+            } else {
+                echo 'An error occured' . $stmt->error;
+            }
+        }
     }
+} else {
+    echo "Your registeration form is not complete!";
 }
