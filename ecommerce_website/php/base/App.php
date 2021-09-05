235e695f236befd1a15fce7bc31b35110058c21c
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-05 14:30:42 +0300

    Create loginUser and registerUser Functions

--- ecommerce_website/php/base/App.php
+++ ecommerce_website/php/base/App.php
@@ -9,44 +9,71 @@ require_once "./Cart.php";
 
 class App
 {
-    public function loginUser($email, $password)
+    public static function loginUser($email, $password)
     {
         session_start();
 
-        if (isser($_SESSION["user"])) {
-            header("location: ../home.php");
+        if (isset($_SESSION["user"])) {
+            header("location: ../../index.html");
         }
 
-        if (isset($_POST["email"]) && isset($_POST["password"])) {
-            $stmt = $connection->prepare("SELECT id, first_name, last_name, email, gender FROM user WHERE email = ? AND password = ?");
-            $stmt->bind_param("ss", $_POST["email"], hash('sha256', $_POST["password"]));
+        $stmt = $connection->prepare("SELECT id, first_name, last_name, email, gender FROM user WHERE email = ? AND password = ?");
+        $stmt->bind_param("ss", $email, hash("sha256", $password));
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($id, $firstName, $lastName, $email, $gender);
+            $stmt->fetch();
+
+            $stmt = $connection->prepare("SELECT id FROM store WHERE user_id = ?");
+            $stmt->bind_param("i", $id);
             $stmt->execute();
 
             $stmt->store_result();
 
             if ($stmt->num_rows > 0) {
-                $stmt->bind_result($id, $firstName, $lastName, $email, $gender);
+                $stmt->bind_result($storeId);
                 $stmt->fetch();
 
-                $stmt = $connection->prepare("SELECT id FROM store WHERE user_id = ?");
-                $stmt->bind_param("i", $id);
-                $stmt->execute();
+                $storeOwner = new StoreOwner($id, $firstName, $lastName, $email, $gender, $storeId);
+                $_SESSION["user"] = $storeOwner;
 
-                $stmt->store_result();
-
-                if ($stmt->num_rows > 0) {
-                    $stmt->bind_result($storeId);
-                    $stmt->fetch();
-
-                    $storeOwner = new StoreOwner($id, $firstName, $lastName, $email, $gender, $storeId);
-                } else {
-                    $user = new User($id, $firstName, $lastName, $email, $gender);
-                }
-
-                header("location: ../home.php");
             } else {
-                echo 'Incorrect username and/or password!';
+                $user = new User($id, $firstName, $lastName, $email, $gender);
+                $_SESSION["user"] = $user;
             }
+
+            header("location: ../../index.html");
+        } else {
+            echo 'Incorrect username and/or password!';
+        }
+    }
+
+    public static function registerUser($firstName, $lastName, $email, $gender, $password)
+    {
+
+        if ($stmt = $connection->prepare('SELECT email FROM user WHERE email = ?')) {
+            $stmt->bind_param('s', $_POST['email']);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if ($stmt->num_rows > 0) {
+                echo ("Email already exists, Try to login");
+                header("refresh:2;url=../../login.html");
+            } else {
+                if ($stmt = $connection->prepare("INSERT INTO user (`email`, `password`, `first_name`, `last_name`, `gender`) VALUES (?, ?, ?, ?, ?)")) {
+                    $stmt->bind_param("sssss", $email, hash("sha256", $password), $firstName, $lastName, $gender);
+                    $stmt->execute();
+                }
+                if ($stmt->affected_rows > 0) {
+                    header("location: ../../login.html");
+                } else {
+                    echo 'An error occured' . $stmt->error;
+                }
+            }
+
         }
     }
 }
