c12d9daf28e96ac1b6536d79df788fa122a5c6a0
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-06 13:17:10 +0300

    Create getProductsOfUserWishList Function

--- ecommerce_website/php/base/Product.php
+++ ecommerce_website/php/base/Product.php
@@ -23,6 +23,31 @@ class Product
         $this->imageHover = $imageHover;
     }
 
+    public static function getProductsOfUserWishlist($wishlistId)
+    {
+        require_once "config/connection.php";
+        $stmt = $connection->query('SELECT product.* FROM product, wishlist_product WHERE product.id = wishlist_product.product_id AND wishlist_product.wishlist_id = ' . $wishlistId);
+
+        while ($row = $stmt->fetch_assoc()) {
+
+            if ($imgQuery = $connection->query('SELECT path FROM image WHERE product_id = ' . $row["id"])) {
+
+                while ($img = $imgQuery->fetch_assoc()) {
+                    $imgs[] = $img["path"];
+                }
+
+            } else {
+                echo $connection->error;
+            }
+
+            $product = new Product($row["id"], $row["name"], $row["description"], $row["price"], $row["quantity"],
+                $row["store_id"], $imgs[0], $imgs[1]);
+
+            $products[$row["id"]] = $product->convertToArray();
+        }
+        return $products;
+    }
+
     public static function getStoreProducts($storeId)
     {
         require_once "config/connection.php";
