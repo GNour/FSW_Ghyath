1736c0146959bc66d19cd8ac181cb526e08a50ec
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-06 12:15:55 +0300

    Create cart and wishlist for user Inside registerUser function

--- ecommerce_website/php/base/App.php
+++ ecommerce_website/php/base/App.php
@@ -80,8 +80,27 @@ class App
                 if ($stmt = $connection->prepare("INSERT INTO user (`email`, `password`, `first_name`, `last_name`, `gender`) VALUES (?, ?, ?, ?, ?)")) {
                     $stmt->bind_param("ssssi", $email, hash("sha256", $password), $firstName, $lastName, $gender);
                     $stmt->execute();
+
                 }
                 if ($stmt->affected_rows > 0) {
+                    $user_id_inserted = $connection->insert_id;
+                    echo $user_id_inserted;
+                    $stmt->close();
+                    if ($cart = $connection->prepare("INSERT INTO `cart` (`user_id`) VALUES (?)")) {
+                        $cart->bind_param("i", $user_id_inserted);
+                        echo $cart->error;
+                        $cart->execute();
+                    } else {
+                        echo 'An error occured' . $cart->error;
+                    }
+
+                    if ($wishlist = $connection->prepare("INSERT INTO `wishlist` (`user_id`) VALUES (?)")) {
+                        $wishlist->bind_param("i", $user_id_inserted);
+                        $wishlist->execute();
+                    } else {
+                        echo 'An error occured' . $wishlist->error;
+                    }
+
                     header("location: ../login.html");
                 } else {
                     echo 'An error occured' . $stmt->error;
