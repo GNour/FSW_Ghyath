c095839224565c9d749036910d47a15e6666dacb
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-08 12:35:00 +0300

    Move getStores() from App.php to Store.php

--- ecommerce_website/php/base/App.php
+++ ecommerce_website/php/base/App.php
@@ -8,21 +8,4 @@ require_once "base/Cart.php";
 class App
 {
 
-    public static function getStores()
-    {
-        require_once "config/connection.php";
-        require_once "base/Store.php";
-
-        $stmt = $connection->query('SELECT * FROM store');
-
-        while ($row = $stmt->fetch_assoc()) {
-            $store = new Store($row["id"], $row["name"], $row["description"], $row["store_image"], $row["store_header_image"],
-                $row["phone_number"], $row["email"], $row["street"], $row["city"], $row["country"], $row["user_id"]);
-
-            $stores[$row["id"]] = $store->convertToArray();
-        }
-
-        return json_encode($stores);
-    }
-
 }
--- ecommerce_website/php/base/Store.php
+++ ecommerce_website/php/base/Store.php
@@ -64,4 +64,21 @@ class Store
         }
     }
 
+    public static function getStores()
+    {
+        require_once "config/connection.php";
+        require_once "base/Store.php";
+
+        $stmt = $connection->query('SELECT * FROM store');
+
+        while ($row = $stmt->fetch_assoc()) {
+            $store = new Store($row["id"], $row["name"], $row["description"], $row["store_image"], $row["store_header_image"],
+                $row["phone_number"], $row["email"], $row["street"], $row["city"], $row["country"], $row["user_id"]);
+
+            $stores[$row["id"]] = $store->convertToArray();
+        }
+
+        return json_encode($stores);
+    }
+
 }
