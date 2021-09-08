b337461a40b5730bb386ee4eda5b5c64a3c7cd07
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-08 14:16:58 +0300

    Fix if wishlist is empty

--- ecommerce_website/scripts/wishlist.js
+++ ecommerce_website/scripts/wishlist.js
@@ -8,39 +8,41 @@ $(function () {
     succes: function (response) {},
     complete: function (response) {
       products = response.responseJSON;
-      for (let [key, product] of Object.entries(products)) {
-        var image = product.image.replace("..", ".");
-        var productHtml = `
-          <tr id="product_${product.id}">
-                  <td class="product-remove text-center" valign="middle">
-                    <a href="javascript:void(0)" id="${product.id}" onclick="removeFromWishlist(this.id)"
-                      ><i class="icon icon anm anm-times-l"></i
-                    ></a>
-                  </td>
-                  <td class="product-thumbnail text-center">
-                    <a href="#"
-                      ><img
-                        src="${image}"
-                        alt="${image}"
-                        title="${product.name}"
-                    /></a>
-                  </td>
-                  <td class="product-name">
-                    <h4 class="no-margin">
-                      <a href="#">${product.name}</a>
-                    </h4>
-                  </td>
-                  <td class="product-price text-center">
-                    <span class="amount">$${product.price}</span>
-                  </td>
-                  <td class="product-subtotal text-center">
-                    <button type="button" onclick="addToCart(${product.id})" class="btn btn-small">
-                      Add To Cart
-                    </button>
-                  </td>
-                </tr>`;
+      if (products != null) {
+        for (let [key, product] of Object.entries(products)) {
+          var image = product.image.replace("..", ".");
+          var productHtml = `
+              <tr id="product_${product.id}">
+                      <td class="product-remove text-center" valign="middle">
+                        <a href="javascript:void(0)" id="${product.id}" onclick="removeFromWishlist(this.id)"
+                          ><i class="icon icon anm anm-times-l"></i
+                        ></a>
+                      </td>
+                      <td class="product-thumbnail text-center">
+                        <a href="#"
+                          ><img
+                            src="${image}"
+                            alt="${image}"
+                            title="${product.name}"
+                        /></a>
+                      </td>
+                      <td class="product-name">
+                        <h4 class="no-margin">
+                          <a href="#">${product.name}</a>
+                        </h4>
+                      </td>
+                      <td class="product-price text-center">
+                        <span class="amount">$${product.price}</span>
+                      </td>
+                      <td class="product-subtotal text-center">
+                        <button type="button" onclick="addToCart(${product.id})" class="btn btn-small">
+                          Add To Cart
+                        </button>
+                      </td>
+                    </tr>`;
 
-        wishlistContainer.append(productHtml);
+          wishlistContainer.append(productHtml);
+        }
       }
     },
   });
